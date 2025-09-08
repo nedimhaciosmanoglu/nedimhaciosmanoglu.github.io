d06bf452785193ecf2bc80c037c93639dbf4fa36
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2025-09-08 14:16:42 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@1bb9aaaf3ff5c3baba1a1c8f51c4a3d3d0f5db82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,10 +32,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please click the PDF button to download my detailed CV file.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",

c30f2106b42f97bc0518ad8e353075def5f9a195
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2025-09-08 11:52:12 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@b450f69c528120b56f56db6f20eb68ef5549f799 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,8 +419,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-finished-my-phd",
-          title: 'Finished my PhD! 🦾 👨‍🔬',
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

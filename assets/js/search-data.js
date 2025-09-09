aaae7f7baa8753a9da347b737c4a5ffae8415fe3
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2025-09-09 19:27:31 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@1678dbbf0cdc2422aca9faf6f9952ec042762bb5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
+  },{id: "nav-lt-strong-gt-blog-lt-strong-gt",
+          title: "&lt;strong&gt;Blog&lt;/strong&gt;",
           description: "",
           section: "Navigation",
           handler: () => {

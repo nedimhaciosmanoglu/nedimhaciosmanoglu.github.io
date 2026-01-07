b2d93086135dae05f1e2b83a61c1cb0591614cda
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2026-01-07 13:27:44 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@09e1ed7b6f1a137374908b198e1ec5e03d8fcae0 🚀

---
+++ _pages/publications.md
@@ -0,0 +1,12 @@
+
+```markdown
+---
+layout: publications
+permalink: /publications/
+title: Publications
+description: Publications by categories in reversed chronological order.
+years: [2024, 2023, 2022, 2021, 2020]
+nav: true
+nav_order: 3
+---
+```
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "Publications",
-          description: "Please refer to my Google Scholar profile for a comprehensive list of my publications, including journal articles, book chapters, and patents.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-news",
+  },{id: "nav-news",
           title: "News",
           description: "",
           section: "Navigation",

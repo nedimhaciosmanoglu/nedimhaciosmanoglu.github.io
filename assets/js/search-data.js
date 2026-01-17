13f83b7e065c300c3bf963135b712c2bb2132313
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2026-01-17 19:04:32 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@9eab26c1ed42c22c9fc19ac1ab6c3d2102284fbc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/quotes/";
           },
+        },{id: "nav-nedo-lab",
+          title: "Nedo Lab",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/quotes/";
+          },
         },{id: "post-lets-start",
         
           title: "Lets Start",

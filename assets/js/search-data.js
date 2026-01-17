e31e536b8b63cd59ecfeb10a2b8f023392849413
nedimhaciosmanoglu <37580402+nedimhaciosmanoglu@users.noreply.github.com>
2026-01-17 19:05:06 +0000

    Deploying to gh-pages from @ nedimhaciosmanoglu/nedimhaciosmanoglu.github.io@0d203f321d3f4e5fcc7f50f20c7412f3a37cd1b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/quotes/";
           },
+        },{id: "nav-mind-snacks",
+          title: "Mind Snacks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/quotes/";
+          },
         },{id: "post-lets-start",
         
           title: "Lets Start",
